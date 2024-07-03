8f110ed4f6c15035835f8e19a40e0913daa521e0
victor-sarti <sartivictor24@gmail.com>
2024-07-03 01:46:44 -0300

    import the  sections for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
 
 const App = () => {
   return (
     <>
     <NavBar/>
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+    <HeroSection/>
+    <FeatureSection/>
+    <WorkFlow/>
+    </div>
     </>
   )
 }
