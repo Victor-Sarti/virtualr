874dedec3258642715670be8d4f89dd4f966b515
victor-sarti <sartivictor24@gmail.com>
2024-07-03 17:44:23 -0300

    import the sections for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
 import WorkFlow from './components/WorkFlow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
     <HeroSection/>
     <FeatureSection/>
     <WorkFlow/>
+    <Pricing/>
+    <Testimonials/>
+    <Footer/>
     </div>
     </>
   )
